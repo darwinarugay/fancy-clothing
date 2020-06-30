b674e1e149227181879d7a2e71c019d6ce3a0091
DaRz <65641146+darwinarugay@users.noreply.github.com>
2020-06-30 17:36:11 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
